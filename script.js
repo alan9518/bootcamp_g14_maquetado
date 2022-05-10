7694a6f91f5d45e430c45f31cce963acdb499d11
LuisBalan <jlbalcz@gmail.com>
2022-05-10 00:19:13 -0500

    first canvas with colored rectangles

---
+++ script.js
@@ -0,0 +1,23 @@
+function draw(){
+    let canvas1 = document.getElementById('stockGraph')
+    if(canvas1.getContext){
+        let ctxCanvas1 = canvas1.getContext('2d');
+        ctxCanvas1.fillStyle = 'rgb(200, 0, 0)';
+        ctxCanvas1.fillRect(10, 10, 50, 50);
+        ctxCanvas1.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctxCanvas1.fillRect(30, 30, 50, 50);
+    };
+};
+
+// function draw() {
+//     var canvas = document.getElementById('tutorial');
+//     if (canvas.getContext) {
+//       var ctx = canvas.getContext('2d');
+//       ctx.fillStyle = 'rgb(200, 0, 0)';
+//     ctx.fillRect(10, 10, 50, 50);
+
+//     ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+//     ctx.fillRect(30, 30, 50, 50);
+    
+//     }
+//   }
