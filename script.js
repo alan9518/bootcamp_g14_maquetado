bb12b8b04cf46a86eddfcb1723f229c1c4da2b48
LuisBalan <jlbalcz@gmail.com>
2022-05-10 00:37:32 -0500

    add some rectangles with fillRect, clearRect and strokeRect

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ function draw(){
     let canvas1 = document.getElementById('stockGraph')
     if(canvas1.getContext){
         let ctxCanvas1 = canvas1.getContext('2d');
-        ctxCanvas1.fillStyle = 'rgb(200, 0, 0)';
-        ctxCanvas1.fillRect(10, 10, 50, 50);
-        ctxCanvas1.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        ctxCanvas1.fillRect(30, 30, 50, 50);
+        // ctxCanvas1.fillStyle = 'rgb(200, 0, 0)';
+        // ctxCanvas1.fillRect(10, 10, 50, 50);
+        // ctxCanvas1.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        // ctxCanvas1.fillRect(30, 30, 50, 50);
+
+        ctxCanvas1.fillRect(25, 25, 100, 100);
+        ctxCanvas1.clearRect(45, 45, 60, 60);
+        ctxCanvas1.strokeRect(50, 50, 50, 50);
+
+
     };
 };
 
